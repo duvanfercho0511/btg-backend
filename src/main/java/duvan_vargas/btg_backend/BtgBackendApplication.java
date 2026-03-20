b72309984ef33c76869e5b01f3172704763f631a
Duvan Ferney Vargas Martinez <duvan2191120@correo.uis.edu.co>
2026-03-20 18:29:56 -0500

    primer commit

---
+++ src/main/java/duvan_vargas/btg_backend/BtgBackendApplication.java
@@ -0,0 +1,13 @@
+package duvan_vargas.btg_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtgBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/duvan_vargas/btg_backend/BtgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package duvan_vargas.btg_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
